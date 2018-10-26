d8847fa1b05c4bdf3622e6ad7a67111a22e72ffa
AnhHQ <AnhHQ1861997@gmail.com>
2018-10-26 21:29:30 +0700

    .

--- app/src/main/java/com/vanduc/renthouse/view/activity/OptionActivity.java
+++ app/src/main/java/com/vanduc/renthouse/view/activity/OptionActivity.java
@@ -1,15 +1,15 @@
-package com.vanduc.renthouse.screens;
+package com.vanduc.renthouse.view.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.vanduc.renthouse.R;
 
-public class OptionMainActivity extends AppCompatActivity {
+public class OptionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_option_main);
+        setContentView(R.layout.activity_option);
     }
 }
